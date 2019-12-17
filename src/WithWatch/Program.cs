d75d29f2429e5c4975b76cd8b00be15b7fa10af7
Aaron Stannard <aaron@petabridge.com>
2019-12-17 02:25:13 -0600

    using dynamic ports

--- src/WithWatch/Program.cs
+++ src/WithWatch/Program.cs
@@ -18,10 +18,6 @@ namespace WithWatch
             var system1 = ActorSystem.Create("System1", ConfigurationFactory.ParseString("akka.remote.dot-netty.tcp.port = 11000")
                 .WithFallback(defaultConfig));
 
-            // bind to port 11001
-            var system2 = ActorSystem.Create("System2", ConfigurationFactory.ParseString("akka.remote.dot-netty.tcp.port = 11001")
-                .WithFallback(defaultConfig));
-
             system1.ActorOf(dsl =>
             {
                 dsl.ReceiveAny((m, ctx) =>
@@ -30,42 +26,56 @@ namespace WithWatch
                 });
             }, "pinger");
 
-            var selection = system2.ActorSelection(new RootActorPath(Address.Parse("akka.tcp://System1@127.0.0.1:11000")) / "user" / "pinger");
-            IActorRef pinger = ActorRefs.NoSender;
-            while (pinger.IsNobody())
+            // bind to port 11001
+            var port = 11002;
+            
+            for(var i = 0; i < 30; i++)
             {
-                try
+                var sys2Config = ConfigurationFactory.ParseString($"akka.remote.dot-netty.tcp.port = {port + i}")
+                .WithFallback(defaultConfig);
+                var system2 = ActorSystem.Create("System2", sys2Config);
+
+                var selection = system2.ActorSelection(new RootActorPath(Address.Parse($"akka.tcp://System1@127.0.0.1:11000")) / "user" / "pinger");
+                IActorRef pinger = ActorRefs.NoSender;
+                while (pinger.IsNobody())
                 {
-                    pinger = await selection.ResolveOne(TimeSpan.FromSeconds(1));
+                    try
+                    {
+                        pinger = await selection.ResolveOne(TimeSpan.FromSeconds(1));
+                    }
+                    catch { }
                 }
-                catch { }
+
+                var watcher = system2.ActorOf(dsl =>
+                {
+                    var sent = false;
+                    dsl.OnPreStart = ctx =>
+                    {
+                        // death watch pinger
+                        ctx.Watch(pinger);
+                    };
+
+                    dsl.Receive<Terminated>((t, ctx) =>
+                    {
+                        ctx.GetLogger().Info("{0} terminated", t);
+                    });
+
+                    dsl.Receive<string>((str, ctx) =>
+                    {
+                        pinger.Forward(str);
+                    });
+                }, "watcher");
+
+                await watcher.Ask<string>("foo", TimeSpan.FromSeconds(1));
+                system2.Log.Info("Received ping back from {0}", system1);
+
+                await system2.Terminate();
+                system2 = null;
             }
 
-            var watcher = system2.ActorOf(dsl =>
-            {
-                var sent = false;
-                dsl.OnPreStart = ctx =>
-                {
-                    // death watch pinger
-                    ctx.Watch(pinger);
-                };
-
-                dsl.Receive<Terminated>((t, ctx) =>
-                {
-                    ctx.GetLogger().Info("{0} terminated", t);
-                });
-
-                dsl.Receive<string>((str, ctx) =>
-                {
-                    pinger.Forward(str);
-                });
-            }, "watcher");
-
-            await watcher.Ask<string>("foo", TimeSpan.FromSeconds(1));
-            system2.Log.Info("Received ping back from {0}", system1);
-
-            await system2.Terminate();
-            await system1.WhenTerminated;
+            Console.ReadLine();
+            await system1.Terminate();
+            Console.ReadLine();
         }
     }
 }
